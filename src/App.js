0e8b73e514a8ed0b4801d5e576f1751ff41349f9
OrestasM <orestas.mazeikaa@gmail.com>
2018-12-04 05:23:26 +0200

    Installed redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import SideBar from './components/SideBar/SideBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <SideBar/>
       </div>
     );
   }
